db8f549b1bb98ae00b46a0971a32ee33a41162ac
Aitor Garcia <agarciap@vicomtech.org>
2016-09-15 17:53:21 +0200

    First bunch of things...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../shared/api.service';
+
+import {AnalysisResponse,Token }from '../model/analysis-response';
+
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
@@ -7,12 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+private result:AnalysisResponse;
+
+  constructor(private apiService:ApiService) {
     // Do stuff
   }
 
   ngOnInit() {
     console.log('Hello Home');
   }
+
+  sendAnalysis(text:string){
+    console.log("sending to analyze: "+text);
+    this.apiService.analyze(text).subscribe(res=>this.result=res);
+  }
+
+getTokenStyle(token:Token):string{
+  if(token.polarityValue.value>0){
+    return "green";
+  }else if(token.polarityValue.value<0){
+    return "red";
+  }else{
+    return "grey";
+  }
+}
 
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { Headers, Http, Response } from '@angular/http';
+
+import { Observable }        from 'rxjs/Observable';
+
+import {AnalysisResponse} from "../model/analysis-response";
+
 @Injectable()
 export class ApiService {
   title = 'Angular 2';
+
+private headers = new Headers({ 'Content-Type': 'application/json' });
+private headersUrlEncoded= new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+
+private analysisUrl="http://localhost:8082/webgira/absa";
+
+constructor(private http: Http) { }
+
+  analyze(text:string):Observable<AnalysisResponse>{
+    return this.http
+    .post(this.analysisUrl,`text=${text}`,{ headers: this.headersUrlEncoded })
+    .map((r: Response) => r.json() as AnalysisResponse);
+  }
+
 }
